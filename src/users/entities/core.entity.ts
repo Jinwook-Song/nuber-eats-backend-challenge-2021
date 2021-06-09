6989ffda40bec36b1ede4d7ee23dbbc9745d5faf
jinwook-song <wlsdnr129@naver.com>
2021-06-09 20:53:28 +0900

    edit core entity2

--- src/users/entities/core.entity.ts
+++ src/users/entities/core.entity.ts
@@ -6,18 +6,18 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity()
+// @Entity()
 @ObjectType()
 export class CoreEntity {
   @PrimaryGeneratedColumn()
-  @Field(type => Int)
+  @Field((type) => Int)
   id: number;
 
   @CreateDateColumn()
-  @Field(type => Date)
+  @Field((type) => Date)
   createdAt: Date;
 
   @UpdateDateColumn()
-  @Field(type => Date)
+  @Field((type) => Date)
   updatedAt: Date;
 }
