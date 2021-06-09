976d31d0b794fbc70f21627f4175b8e8dc506077
jinwook-song <wlsdnr129@naver.com>
2021-06-10 03:17:52 +0900

    Add eager

--- src/podcast/entities/podcast.entity.ts
+++ src/podcast/entities/podcast.entity.ts
@@ -30,17 +30,22 @@ export class Podcast extends CoreEntity {
   @Field(() => User)
   @ManyToOne(() => User, (user) => user.podcasts, {
     onDelete: 'CASCADE',
+    eager: true,
   })
   creator: User;
 
   @RelationId((podcast: Podcast) => podcast.creator)
   creatorId: number;
 
-  @OneToMany(() => Episode, (episode) => episode.podcast)
+  @OneToMany(() => Episode, (episode) => episode.podcast, {
+    eager: true,
+  })
   @Field((type) => [Episode])
   episodes: Episode[];
 
-  @OneToMany(() => Review, (review) => review.podcast)
+  @OneToMany(() => Review, (review) => review.podcast, {
+    eager: true,
+  })
   @Field((type) => [Review])
   reviews: Review[];
 }
